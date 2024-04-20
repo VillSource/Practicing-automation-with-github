ea9f6b84349bbb064a60c19ec5667f842b08846d
VillSource <anirut1800@gmail.com>
2024-04-20 16:27:35 +0700

    update test project

---
+++ Test/VillsourceTests/Featurs/Calculators/ExpressionCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Villsource.Common;
+using Xunit;
+
+namespace Villsource.Featurs.Calculators.Tests;
+
+public class ExpressionCalculatorTests
+{
+
+    [Fact]
+    public void ExcecuteTest()
+    {
+        string expression = @"1+2*3";
+        var expectedResult = 7;
+
+        ICalculator cal = new ExpressionCalculator(expression);
+        var actualResult = cal.Excecute();
+
+        Xunit.Assert.Equal(expectedResult, actualResult);
+    }
+
+}
